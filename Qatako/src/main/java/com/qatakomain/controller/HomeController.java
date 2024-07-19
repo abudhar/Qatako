aadb1bf1b81d64eab9fe2ddf83eca273b9d84bff
Mohammed.abudhar <mohammed.abudhar@anoudtechnologies.com>
2024-07-19 15:57:17 +0530

    no message

--- Qatako/src/main/java/com/qatakomain/controller/HomeController.java
+++ Qatako/src/main/java/com/qatakomain/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HelloController {
+public class HomeController {
 	@RequestMapping(path = "/", method = RequestMethod.GET)
 	public String home() {
 		return "index";
